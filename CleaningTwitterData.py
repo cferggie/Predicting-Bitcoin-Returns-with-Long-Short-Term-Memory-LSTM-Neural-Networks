d90a7b5dfd842bc39aab3e9a6736a43b99a446d7
Brother-bilo <40899599+Brother-bilo@users.noreply.github.com>
2022-04-25 18:51:55 -0400

    Update CleaningTwitterData.py

--- CleaningTwitterData.py
+++ CleaningTwitterData.py
@@ -13,9 +13,20 @@ from nltk.stem.wordnet import WordNetLemmatizer
 #Read in Preprocessed tweets
 tweets = pd.read_csv('CBT.csv', index_col=['Date', 'Tweets'])
 
-#To check what is coming out of tweets
-#Tweets that were tweeted at the same time are grouped together
-#print(tweets)
+#Remove all duplicate tweets (keep the first instance)
+Dup_tweets = tweets[tweets.duplicated()]
+Remove_dups = tweets.drop_duplicates(subset='Tweets', keep='first')
 
-#Start the cleaning process
+#Create function that cleans text
+def cleanTxt(text):
+    text = re.sub(r'@[A-Za-z0-9_]+', '', text) #Removes @ mentions
+    text = re.sub(r'#', '', text) #Remove '#'
+    text = re.sub(r'RT[\s]', '', text) #Removing RT
+    text = re.sub(r'https?:\/\/\S+|www\.\S+', '', text) #Remove links
+    text = re.sub(r'<.*?>', '', text) #Remove HTML code
+    text = re.sub(r'[^ a-zA-Z0-9!.?,;]', '', text) #Removes special characters
 
+    return text
+
+#Clean the text
+tweets['Tweets'] = tweets['Tweets'].apply(cleanTxt)
